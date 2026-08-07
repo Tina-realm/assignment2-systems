f1154ef2406e9d282660e93880860d805ffda529
Yang, Lei <106716395+Tina-realm@users.noreply.github.com>
2026-08-06 23:53:44 -0500

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import torch
 
+from cs336_systems.ddp import DistributedDataParallel
 from cs336_systems.flash_attention import FlashAttention2PyTorch, FlashAttention2Triton
 
 
@@ -49,8 +50,7 @@ def get_ddp(module: torch.nn.Module) -> torch.nn.Module:
     Returns:
         Instance of a DDP class.
     """
-    # For example: return DDP(module)
-    raise NotImplementedError
+    return DistributedDataParallel(module)
 
 
 def ddp_on_after_backward(ddp_model: torch.nn.Module, optimizer: torch.optim.Optimizer):
@@ -64,8 +64,7 @@ def ddp_on_after_backward(ddp_model: torch.nn.Module, optimizer: torch.optim.Opt
         optimizer: torch.optim.Optimizer
             Optimizer being used with the DDP-wrapped model.
     """
-    # For example: ddp_model.finish_gradient_synchronization()
-    raise NotImplementedError
+    ddp_model.finish_gradient_synchronization()
 
 
 def get_fsdp(module: torch.nn.Module, compute_dtype: torch.dtype | None = None) -> torch.nn.Module:
